12e003d340a83fbc06b5ac25bd170cb410bb7f17
Mwadumbo Masoud <68898291+Masoud254@users.noreply.github.com>
2022-11-29 18:04:06 +0300

    Add files via upload

---
+++ introduction.php
@@ -0,0 +1,20 @@
+<?php include 'header.php';?>
+<div class="container">
+
+       <h1 class="title">Introduction</h1>
+       <div class="row">
+              <div class="col-sm-4"><p>At the Anka Beach Resort guestrooms have been specifically designed to provide paramount comfort and luxury for all. Each wing offers a unique ambience ensuring pure relaxation and comfort. The Baobab's accommodation has been designed with luxury in mind and tastefully furnished by Kenya's leading interior designers. All rooms are refined and fitted with all modern amenities with private terrace or balcony, offering a variety of spectacular ocean, pool and garden views. Large inter-connecting rooms are available which are ideal of familes.</p></div>
+              <div class="col-sm-4"><p>Offering world-class hospitality standards, Diani Reef Beach Resort & Spa is the perfect destination for beach holidays, corporate and social events and grand weddings. The resort brings refined cuisine in its multi-cuisine restaurants and bars, as well as relaxation at Maya Spa and state-of-the-art fitness facilities. </p></div>
+              <div class="col-sm-4"><p>The resort animation team keeps guests entertained all day with regular changing animation programs like fashion shows, water gymnastics, bands, discos, acrobatic shows and traditional African folk dancers just to name a few. Some services may carry an extra charge payable locally.</p></div>              
+       </div>
+
+
+       <div class="spacer">
+       <div class="embed-responsive embed-responsive-16by9"><iframe  class="embed-responsive-item" src="//player.vimeo.com/video/55057393?title=0" width="100%" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+       </div>
+
+
+
+
+</div>
+<?php include 'footer.php';?>
